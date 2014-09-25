b04cdedd62a4e6ae3b7ea39fafc568be12e353fe
MauroMombelli <mombelli.mauro@gmail.com>
2014-09-25 22:49:13 +0200

    Create struct.h

---
+++ static_generic/user_data/struct.h
@@ -0,0 +1,19 @@
+/* BEGIN: just some struct to test, fell free to move them on external header file */
+struct A
+{
+	int a1;
+	long a2;
+};
+
+struct B
+{
+	float b1;
+	char b2[6];
+};
+
+struct C
+{
+	unsigned int c1;
+	double c2[5];
+};
+/* END: just some struct to test, fell free to move them on external header file */
