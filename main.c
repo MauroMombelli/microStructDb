c6079c15ed4111b3fbe340ed8867d5a9e1056c52
MauroMombelli <mombelli.mauro@gmail.com>
2014-09-25 22:48:06 +0200

    moving components to outside header

--- main.c
+++ main.c
@@ -2,67 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* BEGIN: just some struct to test, fell free to move them on external header file */
-struct A
-{
-	int a1;
-	long a2;
-};
-
-struct B
-{
-	float b1;
-	char b2[6];
-};
-
-struct C
-{
-	unsigned int c1;
-	double c2[5];
-};
-/* END: just some struct to test, fell free to move them on external header file */
-
-/* this macro will create the right X macro element, and also initiliaze the "anonymous" struct */
-#define ADD_STRUCT_TO_ARRAY(xu) X(xu, &(struct xu){})SEP
-
-/* BEGIN: add or remove your own struct here! fell free to move them on external header file 
- * just need to pass struct name without "struct" to ADD_STRUCT_TO_ARRAY macro */
-#define GENERIC_TABLE \
-	ADD_STRUCT_TO_ARRAY(A) \
-	ADD_STRUCT_TO_ARRAY(B) \
-	ADD_STRUCT_TO_ARRAY(C)
-/* END: add or remove your own struct here! */
-
-/* here we initialize the enum, where the type of the struct is the key, and the value its position in the array */
-#define SEP ,
-#define X(a,b) a
-enum STRUCT {
-	GENERIC_TABLE
-};
-#undef X
-
-/* here we initalize the array of structure */
-#define X(a,b) b
-void * const generic[] =
-{
-	GENERIC_TABLE
-};
-#undef X
-#undef SEP
-
-/* here we create all the getter function. add here your array locking code */
-#define SEP ;
-#define X(a,b) void get_##a(struct a * dest){memcpy(dest, generic[a], sizeof(struct a) );} 
-GENERIC_TABLE
-#undef X
-#undef SEP
-
-/* here we create all the putter function. add here your array locking code */
-#define SEP ;
-#define X(a,b) void put_##a(struct a * source){memcpy(generic[a], source, sizeof(struct a) );}
-GENERIC_TABLE
-#undef X
-#undef SEP
+#include "static_generic/engine.h"
 
 /*run, code, run!*/
 int main()
