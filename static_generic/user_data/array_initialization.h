99d240fbf25e78f6efebcf25f7b8739ba6440089
MauroMombelli <mombelli.mauro@gmail.com>
2014-09-25 22:49:31 +0200

    Create array_initialization.h

---
+++ static_generic/user_data/array_initialization.h
@@ -0,0 +1,5 @@
+/* add your own struct here!
+ * just need to pass struct name without "struct" to ADD_STRUCT_TO_ARRAY macro */
+ADD_STRUCT_TO_ARRAY(A)
+ADD_STRUCT_TO_ARRAY(B)
+ADD_STRUCT_TO_ARRAY(C)
